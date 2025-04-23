5c6b062e39d81213eb496be0d6cca490e4ce0102
Betul Yazici <betulyzc.066@gmail.com>
2025-04-23 20:42:18 +0300

    feat(api): initialize backend structure with models, database config, and Pydantic schemas

---
+++ SpaceShoter_api/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+#Validates API input/output data (Pydantic)
+
+class ScoreCreate(BaseModel):
+    player_name:str
+    score:int
+
+class ScoreOut(ScoreCreate):
+    id:int
+
+    class Config:
+        from_attributes = True
