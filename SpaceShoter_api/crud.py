7fbcee368722eb4a26d4b3cd573495f3005d468d
Betul Yazici <betulyzc.066@gmail.com>
2025-04-27 13:30:49 +0300

    feat: Prevent duplicate usernames and update score if user already exists (Unity and FastAPI API improvements)

--- SpaceShoter_api/crud.py
+++ SpaceShoter_api/crud.py
@@ -3,15 +3,23 @@ from sqlalchemy import String
 from sqlalchemy.orm import Session
 from models import Score
 from schemas import ScoreCreate
+import schemas
 
 
-def save_score(score:ScoreCreate,db:Session):
-    new_score=Score(**score.model_dump()) #converts the incoming Pydantic data into a model object
-    db.add(new_score)
-    db.commit()
-    db.refresh(new_score) 
-    return new_score
-
+def save_score(score: ScoreCreate, db: Session):
+    existing_score = db.query(Score).filter(Score.player_name == score.player_name).first()
+    if existing_score:
+        existing_score.score = score.score
+        db.commit()
+        db.refresh(existing_score)
+        return existing_score
+    else:
+        new_score = Score(**score.model_dump())
+        db.add(new_score)
+        db.commit()
+        db.refresh(new_score)
+        return new_score
+    
 def get_scores(db:Session):
     return db.query(Score).order_by(Score.score.desc()).all()
     
@@ -32,3 +40,12 @@ def delete_score_by_id(id: int, db: Session):
         return {"message": f"Player '{id} - {deleted.player_name}' deleted."}
     else:
         raise HTTPException(status_code=404, detail="Player not found.")
+    
+
+def check_username(username:str, db:Session):
+    user = db.query(Score).filter(Score.player_name == username).first()
+    if user:
+        return schemas.UsernameCheckResponse(exists=True)
+    else:
+        return schemas.UsernameCheckResponse(exists=False)
+
